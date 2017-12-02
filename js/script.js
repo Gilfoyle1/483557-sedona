db64cd844e623477433a2b17c7e6b1ef44f1796f
MRpadovan <kostomarov.e@gmail.com>
2017-12-02 13:54:09 +0300

    commit29 скрыл попап, в чекбоксе больше не пыгают слова, выбор количества людей в ie стал отображаться хорошо

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 	var searchButton = document.querySelector(".button-search");
 	var form = document.querySelector(".section-form");
 
-	form.classList.add("hotel-form-hide");
+	form.classList.add("form-hide");
 
 	searchButton.addEventListener("click", function(){
 	form.classList.toggle("form-hide");
