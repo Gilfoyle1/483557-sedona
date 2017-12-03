aa81bed0f17263a90cbc6302e5906c450357b449
MRpadovan <kostomarov.e@gmail.com>
2017-12-03 20:49:23 +0300

    commit32 выровнял блок с картой, сделал минификацию css  и js.

---
+++ js/script-mini.js
@@ -0,0 +1,1 @@
+var searchButton=document.querySelector(".button-search"),form=document.querySelector(".section-form");form.classList.add("form-hide");searchButton.addEventListener("click",function(){form.classList.toggle("form-hide")});
