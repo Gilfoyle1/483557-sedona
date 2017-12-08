33ba6b3a7e15e8a45669d403c95c144b71f98736
MRpadovan <kostomarov.e@gmail.com>
2017-12-08 13:00:36 +0300

    Merge remote-tracking branch 'origin/master'

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var button = document.querySelector(".button-search");
 			
 var form = document.querySelector(".section-form");
@@ -10,13 +9,3 @@ button.addEventListener( "click", function (event) {
 	form.classList.toggle("section-form-show");
 	arrival.focus();
 } )
-=======
-	var searchButton = document.querySelector(".button-search");
-	var form = document.querySelector(".section-form");
-
-	form.classList.add("form-hide");
-
-	searchButton.addEventListener("click", function() {
-	    form.classList.toggle("form-hide");
-	});
->>>>>>> be2e4bb07bd56ec7ae535e1462523d4df5ee3967
