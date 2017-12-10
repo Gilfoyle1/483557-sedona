238b1c3234d6a27280cc1f6da3ea105bc5ebd2ed
MRpadovan <kostomarov.e@gmail.com>
2017-12-10 13:02:25 +0300

    commit43 правки после втрой проверки

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ var button = document.querySelector(".button-search");
 			
 var form = document.querySelector(".section-form");
 			
-var arrival = document.querySelector("[name=arrival-date]")
+var arrival = document.querySelector("[name=arrival-date]");
 			
 button.addEventListener( "click", function (event) {
 	event.preventDefault();
 	form.classList.toggle("section-form");
 	arrival.focus();
-} )
+} );
